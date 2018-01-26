45b29dedef3af6a128717fba98306acb3a4a284a
MZeit <33051323+MZeit@users.noreply.github.com>
2018-01-26 13:31:16 +0200

    Update StringCalculator.fs

--- StringCalculator.fs
+++ StringCalculator.fs
@@ -3,3 +3,20 @@ let Add (inputString: string) =
     inputString.Split([|",";"\n"|], StringSplitOptions.RemoveEmptyEntries)
     |> Seq.map int
     |> Seq.sum
+
+// 4. 
+open System.Text.RegularExpressions
+open System
+
+let getStringWithNumbers (wholeString: string) = wholeString.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries).[1]
+
+let splitAndSum (del: string []) (numbersToSum: string) = 
+    numbersToSum.Split(del, StringSplitOptions.RemoveEmptyEntries) 
+    |> Seq.ofArray 
+    |> Seq.map int 
+    |> Seq.sum
+
+let Add stringWithDelimiteter = 
+    match stringWithDelimiteter with 
+    | x when Regex("//.\n.*").Match(x).Success -> (splitAndSum [|stringWithDelimiteter.[2..2]|] (getStringWithNumbers x))
+    | y when Regex("-?[0-9]*.*").Match(y).Success -> (splitAndSum [|",";"\n"|] y)
