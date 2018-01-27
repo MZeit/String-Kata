b234cf7f7b573ab96441d18704fe8d00c5400ec1
MZeit <33051323+MZeit@users.noreply.github.com>
2018-01-27 12:27:36 +0200

    Update StringCalculator.fs
    Added some exception handling

--- StringCalculator.fs
+++ StringCalculator.fs
@@ -11,10 +11,14 @@ open System
 let getStringWithNumbers (wholeString: string) = wholeString.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries).[1]
 
 let splitAndSum (del: string []) (numbersToSum: string) = 
-    numbersToSum.Split(del, StringSplitOptions.RemoveEmptyEntries) 
-    |> Seq.ofArray 
-    |> Seq.map int 
-    |> Seq.sum
+    try
+        numbersToSum.Split(del, StringSplitOptions.RemoveEmptyEntries) 
+        |> Seq.ofArray 
+        |> Seq.map int 
+        |> Seq.sum
+     with
+        | :? System.OverflowException as ex ->  printfn "The number entered is too big or too small.";0
+        | :? System.FormatException as ex -> printfn "Invalid Input String Format."; 0 
 
 let Add stringWithDelimiteter = 
     match stringWithDelimiteter with 
