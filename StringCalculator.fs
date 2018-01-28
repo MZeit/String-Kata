1ee47eaad3f87da543711be79c5623935a7ade1d
MZeit <33051323+MZeit@users.noreply.github.com>
2018-01-28 21:24:29 +0200

    Update StringCalculator.fs

--- StringCalculator.fs
+++ StringCalculator.fs
@@ -1,12 +1,24 @@
+//1-3
 open System
 let Add (inputString: string) =
     inputString.Split([|",";"\n"|], StringSplitOptions.RemoveEmptyEntries)
     |> Seq.map int
     |> Seq.sum
 
-// 4. 
+// 4- 
 open System.Text.RegularExpressions
 open System
+
+exception NegativeNumberException of seq<int>
+
+let checkNegative (seqWithNumbers:seq<int>) = 
+    let negatives = Seq.where (fun b -> (b < 0 )) seqWithNumbers;
+    if (Seq.length negatives) = 0
+    then 
+      seqWithNumbers
+    else
+      raise (NegativeNumberException negatives)
+      Seq.empty
 
 let getStringWithNumbers (wholeString: string) = wholeString.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries).[1]
 
@@ -15,10 +27,12 @@ let splitAndSum (del: string []) (numbersToSum: string) =
         numbersToSum.Split(del, StringSplitOptions.RemoveEmptyEntries) 
         |> Seq.ofArray 
         |> Seq.map int 
+        |> checkNegative
         |> Seq.sum
      with
         | :? System.OverflowException as ex ->  printfn "The number entered is too big or too small.";0
         | :? System.FormatException as ex -> printfn "Invalid Input String Format."; 0 
+        | NegativeNumberException seqWithNegatives -> printfn "Negatives not allowed %A" seqWithNegatives;0
 
 let Add stringWithDelimiteter = 
     match stringWithDelimiteter with 
